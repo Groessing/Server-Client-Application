a26102b8e063fb20190aecbbc07281be48a0b3be
Larissa Grössing <larissagroessing@gmail.com>
2025-10-07 18:34:18 +0200

    Add files via upload

---
+++ Socket.Client/ClientClass.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logging;
+using Socket.Communication;
+using UI;
+namespace Socket.Client
+{
+    public class ClientClass
+    {
+        ISetData datahandler = new ClientDataHandler();
+        private System.Net.Sockets.Socket _clientSocket;
+        ILogWriter _logWriter;
+        public ClientClass(string serverIP, int serverPort, ILogWriter logwriter = null)
+        {
+            _logWriter = logwriter ?? new DefaultLogWriter();
+            _clientSocket = new System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
+            _clientSocket.Connect(serverIP, serverPort);
+            _logWriter.LogServerInfo("Client connected to server...");
+           
+        }
+        //public void SendMessage(string message, int count)
+        //{
+        //    try
+        //    {
+        //        // string request = $"{message}:{count}";
+        //        byte[] requestbytes = Encoding.UTF8.GetBytes(message);
+        //        _clientSocket.Send(requestbytes);
+             
+        //    }
+           
+
+        //    catch (Exception ex)
+        //    {
+        //       _logWriter.LogServerInfo($"Error sending message: {ex.Message}");
+        //    }
+
+        //}
+
+        //public void ReceiveMessage()
+        //{
+        //    byte[] buffer = new byte[1024];
+        //    int received = _clientSocket.Receive(buffer);
+        //    string response = Encoding.UTF8.GetString(buffer, 0, received);
+        //    Console.WriteLine($"Response from server: {response}");
+        //    _logWriter.LogRequestInfo($"Received response: {response}");
+           
+        //}
+
+        public string RequestResponse(string req)
+        {
+            try
+            {
+                // string request = $"{message}:{count}";
+                byte[] requestbytes = Encoding.UTF8.GetBytes(req);
+                _clientSocket.Send(requestbytes);
+
+                byte[] buffer = new byte[1024];
+                int received = _clientSocket.Receive(buffer);
+                string response = Encoding.UTF8.GetString(buffer, 0, received);
+                Console.WriteLine($"Response from server: {response}");
+                _logWriter.LogRequestInfo($"Received response: {response}");
+                return response;
+            }
+
+
+            catch (Exception ex)
+            {
+                _logWriter.LogServerInfo($"Error sending message: {ex.Message}");
+                return null;
+            }
+            
+        }
+
+        public void Close()
+        {
+            _clientSocket.Close();
+            _logWriter.LogServerInfo("Client socket closed.");
+        }
+    }
+}
---
+++ Socket.Client/Program.cs
@@ -0,0 +1,65 @@
+﻿using Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI;
+
+namespace Socket.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            //string serverIP = "127.0.0.1"; 
+            //int serverPort = 5000;         
+            
+            DefaultLogWriter logWriter = new DefaultLogWriter();
+            try
+            {
+                Console.Write("IP Address: ");
+                string serverIP = Console.ReadLine();
+                Console.Write("Port: ");
+                int serverPort = Convert.ToInt32(Console.ReadLine());
+                ClientClass client = new ClientClass(serverIP, serverPort);
+                logWriter.LogServerInfo("Connected to the server...");
+
+                while (true)
+                {
+                    Console.Write("Enter your message (or '#exit#' to quit): ");
+                    string message = Console.ReadLine();
+
+                    if (message?.ToLower() == "#exit#")
+                    {
+                        break;
+                    }
+
+                    //Console.Write("Enter the count: ");
+                    //if (!int.TryParse(Console.ReadLine(), out int count))
+                    //{
+                    //    logWriter.LogRequestInfo("Invalid count entered.");
+                    //    Console.WriteLine("Invalid count. Please enter a number.");
+                    //    continue;
+                    //}
+
+                   
+                    client.RequestResponse(message);
+                }
+
+                client.Close();
+                logWriter.LogServerInfo("Disconnected from the server.");
+            }
+            catch (Exception ex)
+            {
+                logWriter.LogServerInfo($"Error: {ex.Message}");
+            }
+        }
+          
+
+
+        
+    }
+}
