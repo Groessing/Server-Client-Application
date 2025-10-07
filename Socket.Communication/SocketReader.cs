b3dd7f0988f97978d34750ccefe57ba85f4d9d56
Larissa Grössing <larissagroessing@gmail.com>
2025-10-07 18:34:43 +0200

    Add files via upload

---
+++ Socket.Communication/SocketReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socket.Communication
+{
+    public class SocketReader
+    {
+
+        public SocketReader()
+        {
+
+        }
+
+        public string Read(System.Net.Sockets.Socket socket)
+        {
+            byte[] buffer = new byte[1024];
+            int received = socket.Receive(buffer);
+            return Encoding.UTF8.GetString(buffer, 0, received);
+        }
+
+        public void Read(System.Net.Sockets.Socket socket, byte[] buffer)
+        {
+            socket.Receive(buffer);
+        }
+    }
+}
---
+++ Socket.Communication/SocketWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+namespace Socket.Communication
+{
+    public class SocketWriter
+    {
+        public SocketWriter()
+        {
+
+        }
+
+        public void Write(System.Net.Sockets.Socket socket, string text)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(text);
+            socket.Send(data);
+        }
+
+        public void Write(System.Net.Sockets.Socket socket, byte[] buffer)
+        {
+            socket.Send(buffer);
+        }
+    }
+
+
+}
