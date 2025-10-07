35c442746e37c8a664dbfe919e4851f5f858668b
Larissa Grössing <larissagroessing@gmail.com>
2025-10-07 18:32:54 +0200

    Add files via upload

---
+++ Logging/FileWriter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Logging
+{
+    public class FileWriter : ILogWriter
+    {
+        public void LogServerInfo(string message)
+        {
+            File.AppendAllText("serverinfo", message);
+        }
+
+        public void LogRequestInfo(string message)
+        {
+            Console.WriteLine("requestinfo", message);
+        }
+    }
+}
---
+++ Logging/ILogWriter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public interface ILogWriter
+    {
+        void LogServerInfo(string message);
+
+        void LogRequestInfo(string message);
+    }
+}
