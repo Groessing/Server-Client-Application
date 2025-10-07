f0f16710e4264ba13f19f9531eff4a17be67b4e6
Larissa Grössing <larissagroessing@gmail.com>
2025-10-07 18:35:48 +0200

    Add files via upload

---
+++ UI/ClientDataHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    public class ClientDataHandler : ISetData
+    {
+        public void SetReceivedDataGUI(string information)
+        {
+            Console.WriteLine($"[GUI Update]: {information}");
+        }
+
+        public void SetReceivedDataCUI(string information)
+        {
+            Console.WriteLine($"[CUI Update]: {information}");
+        }
+    }
+}
---
+++ UI/ServerDataHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    internal class ServerDataHandler
+    {
+        public void SetReceivedDataGUI(string information)
+        {
+            Console.WriteLine($"[Server GUI]: {information}");
+        }
+
+        public void SetReceivedDataCUI(string information)
+        {
+            Console.WriteLine($"[Server CUI]: {information}");
+        }
+    }
+}
